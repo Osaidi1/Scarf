85db8e380c413c3d72f056a49a707cc0a627e5e2
Osaidi1 <hassanosaid123@gmail.com>
2026-02-25 22:19:22 +0100

    second trap done

--- scripts/falling_trap.gd
+++ scripts/falling_trap.gd
@@ -5,28 +5,30 @@ var current_speed = 0
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var marker_2d: Marker2D = $Marker2D
-@onready var detetector: Area2D = $detect/DETETECTOR
-@onready var hurtbox: CollisionPolygon2D = $hurt/hurt/CollisionShape2D
+@onready var detetector: Area2D = $detector
+@onready var hurtbox: CollisionPolygon2D = $hurt/hurt/collision
 @onready var hurt: Node2D = $hurt
 @onready var sprite_2d: Sprite2D = $hurt/Sprite2D
 
 func _ready() -> void:
 	detetector.monitoring = true
-	$"../Node2D3/hurt/hurt/CollisionShape2D".disabled = true
+	$hurt/hurt/collision.disabled = true
 
 func _physics_process(delta: float) -> void:
 	hurt.position.y += current_speed * delta
-	if hurt.position.y >= marker_2d.position.y - 6:
+	if hurt.position.y >= marker_2d.position.y + 30:
 		current_speed = 0
-		hurt.position.y = marker_2d.position.y - 6
-		$"../Node2D3/hurt/hurt/CollisionShape2D".disabled = false
-		
+		await get_tree().create_timer(0.01).timeout
+		hurt.position.y = marker_2d.position.y + 30
+		$hurt/hurt/collision.disabled = false
 
-func _on_playerdetect_body_entered(body: Node2D) -> void:
+func _on_detector_body_entered(body: Node2D) -> void:
 	if body is Player:
 		animation_player.play("shakeandfall")
+		print("here")
 
 func fall():
+	print("here")
 	current_speed = speed
 
 func destroy():
